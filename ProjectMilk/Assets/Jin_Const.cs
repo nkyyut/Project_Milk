1661dd56eb7342e3395d4ae3e2276663edd8eca1
DESKTOP-0ARPSR5\jin <tamanaha1205@gmail.com>
2019-04-16 13:22:15 +0900

    Meshの生成

---
+++ ProjectMilk/Assets/Jin_Const.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jin_Const
+{
+    public static class _JinConst
+	{
+  		public const int ORDERTRIANGLES_NUM = (6);
+        public const int ORDERTRIANGLES_PORIGON = (3);
+	}
+
+}
