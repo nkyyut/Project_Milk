9a48ec2100301d26fc36d3ba5e9c3516e9f75149
kazuki24 <kazu2324ki@gmail.com>
2019-04-18 12:07:40 +0900

    ミニマップ作成

---
+++ ProjectMilk/Assets/Script/MiniMap.cs
@@ -0,0 +1,30 @@
+﻿//かずき
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+    [SerializeField] float offset;
+
+
+    void Start()
+    {
+        transform.position = PlayerTransform.up * offset;
+        transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+
+
+    void Update()
+    {
+        transform.position = PlayerTransform.position + PlayerTransform.up * offset;
+        transform.rotation = PlayerTransform.rotation;
+        transform.Rotate(90, 0, 0);
+
+
+
+    }
+}
