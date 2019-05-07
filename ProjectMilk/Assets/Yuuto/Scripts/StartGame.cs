df991a5457bdea1e1142f77a6bd2a7bbaf459a15
LAPTOP-LSSKFDV4\nkyyut <nktyut88@gmail.com>
2019-05-07 15:47:42 +0900

    [更新]現状を保存します

---
+++ ProjectMilk/Assets/Yuuto/Scripts/StartGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+    public ParticleSystem bubbleEffects;
+    private float waitTime;
+    private bool startFLG;
+    public AudioSource BGMManager;
+    public AudioClip bubbleSe;
+
+	// Use this for initialization
+	void Start () {
+        waitTime = 0.0f;
+        startFLG = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (startFLG)
+        {
+            waitTime += Time.deltaTime;
+        }
+
+        if(waitTime > 3)
+        {
+            FadeManager.Instance.LoadScene("GameMainScene", 0.5f);
+        }
+	}
+
+    /// <summary>
+    /// スタートボタンを押した時に、
+    /// バブルのエフェクトを起動させ、
+    /// エフェクト終了時にシーンを遷移させる処理
+    /// </summary>
+    public void startGame()
+    {
+        if (!startFLG)
+        {
+            BGMManager.PlayOneShot(bubbleSe);
+            bubbleEffects.Play();
+            startFLG = true;
+        }
+    }
+}
