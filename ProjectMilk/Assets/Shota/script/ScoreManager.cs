36b34a8ddee04358b1eec86570a9ff952c53e92b
yonahasi0911 <shota-y-17d@stu.kbc.ac.jp>
2019-05-28 15:15:46 +0900

    ある日僕はちくわの中を覗いてしまった 　________________________________________________________ （リザルト簡易実装しました）　　　　　ちくわ　　　　　　　　　　　　　） 　‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾

---
+++ ProjectMilk/Assets/Shota/script/ScoreManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    private int nowOniCount;
+    private int oldOniCount;
+
+    [SerializeField] OniCount oniCount;
+
+    private int TotalScore;
+
+    private void Start()
+    {
+        nowOniCount = oldOniCount = oniCount.Count();
+        TotalScore = 0;
+    }
+
+    private void Update()
+    {
+        nowOniCount = oniCount.Count();
+        if(nowOniCount != oldOniCount)
+        {
+           TotalScore += AddScore(oldOniCount - nowOniCount);
+           oldOniCount = nowOniCount;
+        }
+    }
+
+    private int AddScore(int difference)
+    {
+        int rank;
+        int score = 0;
+        if(difference < 4)
+        {
+            rank = 0;
+        }else if(difference < 6)
+        {
+            rank = 1;
+        }
+        else
+        {
+            rank = 2;
+        }
+
+        switch (rank)
+        {
+            case 0:
+                score = difference * 10;
+                break;
+            case 1:
+                score = difference * 20;
+                break;
+            case 2:
+                score = difference * 30;
+                break;
+            default:
+                break;
+        }
+        return score;
+    }
+
+    public int GetOnihitodeScore()
+    {
+        return TotalScore;
+    }
+}
