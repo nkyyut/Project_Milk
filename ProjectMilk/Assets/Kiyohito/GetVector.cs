80659abdc8e2b654bc38e0d835ab7d39dde853a5
YogiKiyohito <kiyohito-y-17d@stu.kbc.ac.jp>
2019-05-28 12:41:50 +0900

    Tag選べるようにしたよ

---
+++ ProjectMilk/Assets/Kiyohito/GetVector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetVector : MonoBehaviour {
+
+    public GameObject Camera;
+    public GameObject Astar;
+    Quaternion InitRotate;
+    Vector3 TargetPoint;
+    float RotateSpeed=5.0f;
+
+	// Use this for initialization
+	void Start () {
+        InitRotate = Astar.transform.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Astar.transform.rotation = Quaternion.Slerp(Astar.transform.rotation, Camera.transform.rotation, Time.deltaTime * RotateSpeed);
+    }
+}
