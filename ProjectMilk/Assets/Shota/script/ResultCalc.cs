adb571556e60b1e8fd0141f54e8faf5e28dac793
DESKTOP-0ARPSR5\jin <tamanaha1205@gmail.com>
2019-05-31 11:38:04 +0900

    ゲームクリア時のBGMを追加しました

--- ProjectMilk/Assets/Shota/script/ResultCalc.cs
+++ ProjectMilk/Assets/Shota/script/ResultCalc.cs
@@ -1,27 +1,35 @@
-﻿using System.Collections;
+﻿// 5/31 玉那覇臣　BGM切り替え
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResultCalc : MonoBehaviour {
+public class ResultCalc : MonoBehaviour
+{
 
     public GameObject ResultManager;
     public GameObject ResultPanel;
+    public GameObject _BGMManager;
 
     public OniCount onicount;
     public TimeManager timemanager;
     public DurableValueManager durablevaluemanager;
     public ScoreManager scoremanager;
-    
+
+    [SerializeField] private AudioClip _ResultBGM;
+
+    private AudioSource _audiosource_BGM;
+
     private bool resultflg;
 
     private void Start()
     {
         resultflg = false;
+        _audiosource_BGM = _BGMManager.GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        if(onicount.Count() == 0)
+        if (onicount.Count() == 0)
         {
             if (!resultflg)
             {
@@ -30,6 +38,8 @@ public class ResultCalc : MonoBehaviour {
                 ResultManager.GetComponent<ResultScript>().Score = GetResultScore();
                 resultflg = true;
                 Debug.Log(GetResultScore());
+                _audiosource_BGM.Stop();
+                Invoke("OnResultBGM" , 2.0f);
             }
         }
     }
@@ -38,11 +48,17 @@ public class ResultCalc : MonoBehaviour {
     {
         float Score = 0;
         // スコア　＝　オニヒトデ討伐ポイント+-*/　残り時間　+-*/　のこりHP  
-        Debug.Log("討伐P"+scoremanager.GetOnihitodeScore());
+        Debug.Log("討伐P" + scoremanager.GetOnihitodeScore());
         Debug.Log("残り時間" + timemanager.GetScoreTime());
         Debug.Log("のこりHP" + durablevaluemanager.GetDurableValue());
         Score = scoremanager.GetOnihitodeScore() + timemanager.GetScoreTime() + durablevaluemanager.GetDurableValue();
 
         return (int)Score;
+    }
+
+    public void OnResultBGM()
+    {
+        _audiosource_BGM.clip = _ResultBGM;
+        _audiosource_BGM.Play();
     }
 }
